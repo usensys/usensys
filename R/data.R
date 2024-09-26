b7114eecb33a6884b72027cd8779ecb42377cc3c
olugovoy <olugovoy@gmail.com>
2024-09-26 18:50:41 -0400

    +26-zone map

---
+++ R/data.R
@@ -0,0 +1,12 @@
+# Documentation of the data objects in the package.
+
+
+#' GIS/map-objects for alternative versions of the model.
+#'
+#' @format A list of sf-objects:
+#' \describe{
+#'   \item{us26_sf}{26-region map}
+#' }
+#' @examples
+#' plot(usensys_maps$us26_sf["region"])
+"usensys_maps"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,15 @@
-## code to prepare `DATASET` dataset goes here
+## Source this file to rebuild the package datasets
 
-usethis::use_data(DATASET, overwrite = TRUE)
+## maps ####
+library(sf)
+usensys_maps <- list()
+
+## 26-zones (MIP/PowerGenome)
+(load("data-raw/us26_sf.RData"))
+usensys_maps$us26_sf <- us26_sf
+
+
+## Save the datasets ####
+usethis::use_data(
+  usensys_maps,
+  overwrite = TRUE, internal = FALSE)
